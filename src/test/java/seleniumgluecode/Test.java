545bdca8c037ec496a1921a085d83393593bd51e
Chrisxavix <chrischillogallid64@outlook.com>
2021-08-26 09:04:50 -0500

    feat: init base test

---
+++ src/test/java/seleniumgluecode/Test.java
@@ -0,0 +1,45 @@
+package seleniumgluecode;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.bson.Document;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Test {
+
+    private ChromeDriver driver;
+
+    @Given("^El usuario se encuentra en la pagina Home de imalittletester$")
+    public void elUsuarioSeEncuentraEnLaPaginaHomeDeImalittletester() throws Throwable {
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://imalittletester.com");
+    }
+
+    @When("^Hace click sobre el titulo The little tester comics$")
+    public void haceClickSobreElTituloTheLittleTesterComics() throws Throwable {
+        driver.findElement(By.id("menu-item-2008")).click();
+    }
+
+    @Then("^Se debe redirigir a la pantalla Comics$")
+    public void seDebeRedirigirAlaPantallaComics() throws Throwable {
+        WebElement pageTitle = driver.findElement(By.className("page-title"));
+        Assert.assertTrue("Error", pageTitle.isDisplayed());
+        Assert.assertEquals("Category: comics", pageTitle.getText());
+    }
+}
